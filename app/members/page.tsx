4d71457907f160d3d723b38b6a3ffc6ba410683f
허정학(90190291) <wtoyatoxato@wooribank.com>
2025-01-03 16:46:24 +0900

    index

--- app/members/page.tsx
+++ app/members/page.tsx
@@ -1,14 +1,15 @@
-'use client'
-
-import { Button } from "@/components/ui/button";
-import { useRouter } from 'next/navigation'
+'use client';
+import React from 'react';
 
 export default function Home() {
-  const router = useRouter()
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>Members</h1>
-      <Button onClick={() => router.push('/games')}>Click me</Button>
-    </div>
+    <form>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <label className="Label" htmlFor="c1">
+          Accept terms and conditions.
+        </label>
+      </div>
+    </form>
   );
 }
+
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,150 @@
-export default function Home() {
+'use client';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { format } from 'date-fns';
+import { CalendarIcon } from 'lucide-react';
+import { useForm, ControllerRenderProps } from 'react-hook-form';
+import { z } from 'zod';
+
+import { cn } from '@/lib/utils';
+import { toast } from '@/hooks/use-toast';
+import { Button } from '@/components/ui/button';
+import { Calendar } from '@/components/ui/calendar';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import { useState } from 'react';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+
+const FormSchema = z.object({
+  date: z.date({
+    required_error: '날짜를 입력해주세요.',
+  }),
+  courtName: z.string({
+    required_error: '코트명을 입력해주세요.',
+  }),
+  courtCount: z.string().optional(),
+});
+
+type OnSelectHandler = (select: Date | undefined, field: ControllerRenderProps<z.infer<typeof FormSchema>>) => void;
+
+export default function CalendarForm() {
+  const [popoverOpen, setpopoverOpen] = useState(false);
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+  });
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    console.log(data);
+    toast({
+      title: 'You submitted the following values:',
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    });
+  }
+
+  const handleDatePicker: OnSelectHandler = (select, field) => {
+    field.onChange(select);
+    setpopoverOpen(false);
+  };
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-     
-    </div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 w-[360px] p-8">
+        <FormField
+          control={form.control}
+          name="date"
+          render={({ field }) => (
+            <FormItem className="flex flex-col">
+              <FormLabel>날짜 입력</FormLabel>
+              <Popover open={popoverOpen}>
+                <PopoverTrigger asChild>
+                  <FormControl>
+                    <Button
+                      variant={'outline'}
+                      className={cn('pl-3 text-left font-normal', !field.value && 'text-muted-foreground')}
+                      onClick={() => setpopoverOpen(true)}
+                    >
+                      {field.value ? format(field.value, 'yyyy.MM.dd') : <span>Pick a date</span>}
+                      <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                    </Button>
+                  </FormControl>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0" align="start">
+                  <Calendar
+                    mode="single"
+                    selected={field.value}
+                    onSelect={(select) => handleDatePicker(select, field)}
+                    disabled={(date) => {
+                      const today = new Date();
+                      const yesterday = new Date(today);
+                      yesterday.setDate(today.getDate() - 1);
+                      return date < yesterday;
+                    }}
+                    initialFocus
+                  />
+                </PopoverContent>
+              </Popover>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="courtName"
+          render={({ field }) => (
+            <FormItem className="flex flex-col">
+              <FormLabel>코트 이름</FormLabel>
+              <Select
+                onValueChange={(select) => {
+                  field.onChange(select);
+                  console.log(select);
+                }}
+                defaultValue={field.value}
+              >
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="코트를 선택해주세요" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectItem value="그랜드슬램">그랜드슬램</SelectItem>
+                  <SelectItem value="망원한강공원">망원한강공원</SelectItem>
+                  <SelectItem value="직접입력">직접입력</SelectItem>
+                </SelectContent>
+              </Select>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="courtCount"
+          render={({ field }) => (
+            <FormItem className="flex flex-col">
+              <FormLabel>코트 수</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="코트 수를 선택하세요." />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectItem value="1">1</SelectItem>
+                  <SelectItem value="2">2</SelectItem>
+                  <SelectItem value="3">3</SelectItem>
+                  <SelectItem value="4">4</SelectItem>
+                </SelectContent>
+              </Select>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
   );
 }
+
